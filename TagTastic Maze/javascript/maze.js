4f13d38a917f30458b4c40d0fca863e318b99c65
Rafiul100 <72925150+Rafiul100@users.noreply.github.com>
2020-10-29 14:19:08 +0000

    Add files via upload

---
+++ TagTastic Maze/javascript/maze.js
@@ -0,0 +1,112 @@
+function azwer1() {
+bomb2.src = "img/powerups/bomb2.png";
+}
+setInterval("azwer1();", 1000);
+
+
+function azwer2() {
+bomb2.src = "img/powerups/shield.svg";
+}
+setInterval("azwer2();", 2000);
+
+
+function azwer3() {
+shield2.src = "img/powerups/shield.svg";
+}
+setInterval("azwer3();", 1000);
+
+
+function azwer4() {
+shield2.src = "img/powerups/bomb2.png";
+}
+setInterval("azwer4();", 2000);
+
+
+
+
+
+
+
+
+
+
+function azwer5() {
+radar2.src = "img/powerups/radar.svg";
+}
+setInterval("azwer5();", 1000);
+
+
+function azwer6() {
+radar2.src = "img/powerups/ice-cube.svg";
+}
+setInterval("azwer6();", 2000);
+
+
+function azwer7() {
+cube2.src = "img/powerups/ice-cube.svg";
+}
+setInterval("azwer7();", 1000);
+
+
+function azwer8() {
+cube2.src = "img/powerups/radar.svg";
+}
+setInterval("azwer8();", 2000);
+
+
+
+
+
+
+
+function azwer9() {
+jump2.src = "img/powerups/jump.svg";
+}
+setInterval("azwer9();", 1000);
+
+
+function azwer10() {
+jump2.src = "img/powerups/time.svg";
+}
+setInterval("azwer10();", 2000);
+
+
+function azwer11() {
+time2.src = "img/powerups/time.svg";
+}
+setInterval("azwer11();", 1000);
+
+
+function azwer12() {
+time2.src = "img/powerups/jump.svg";
+}
+setInterval("azwer12();", 2000);
+
+
+
+
+
+
+
+function azwer13() {
+mystery2.src = "img/powerups/mystery.svg";
+}
+setInterval("azwer13();", 1000);
+
+
+function azwer14() {
+mystery2.src = "img/powerups/flash.svg";
+}
+setInterval("azwer14();", 2000);
+
+
+function azwer15() {
+flash2.src = "img/powerups/flash.svg";
+}
+setInterval("azwer15();", 1000);
+
+
+function azwer16() {
+flash2.src = "img/powerups/mystery.svg";
+}
+setInterval("azwer16();", 2000);
